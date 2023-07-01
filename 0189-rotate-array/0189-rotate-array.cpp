12e392e96625180ef3992e89d3952fd835aeb532
Abhishek garg <98762746+AbhishekGargcoder@users.noreply.github.com>
2023-07-01 16:35:24 +0530

    Time: 28 ms (92.88%), Space: 24.8 MB (88.66%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rev(vector<int>&arr,int l,int h){
+        while(l<h){
+            swap(arr[l++],arr[h--]);
+        }
+    }
+    void rotate(vector<int>&arr, int k) {
+        int n = arr.size();
+        k = k%n;
+        // [4,3,2,1,7,6,5]--> [5,6,7,2,3,4]
+        if(n==1) return;
+        rev(arr,0,n-1-k);
+        rev(arr,n-k,n-1);
+        rev(arr,0,n-1);
+    }
+};
