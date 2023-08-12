470c9fd9d8cca2fee075401dd1f604383e37b534
Abhishek garg <98762746+AbhishekGargcoder@users.noreply.github.com>
2023-08-12 18:59:09 +0530

    Time: 15 ms, Memory: 12.2 MB - LeetHub

---
+++ 5356-78-583-delete-operation-for-two-strings/5356-78-583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+ string rev(string str){
+      int l=0, h = str.length()-1;
+      while(l<h)
+        swap(str[l++],str[h--]);
+        return str;
+  }
+//ion to find the length of longest common subsequence in two strings.
+    int lcs(int n, int m, string s1, string s2)
+    {
+        dp= vector<vector<int>>(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 or j==0){ dp[i][j]=0; continue;}
+                if(s1[i-1]==s2[j-1])
+                  dp[i][j]= 1+ dp[i-1][j-1];
+                else dp[i][j]= max(dp[i-1][j], dp[i][j-1]);
+                
+            }
+        }
+         return dp[n][m];
+    }
+    int longestPalinSubseq(string s) {
+      return lcs(s.length(),s.length(),s,rev(s));
+    }
+  int minDistance(string s1, string s2) {
+     int n= s1.length(),m=s2.length();
+     return n+ m - 2* lcs(n, m,s1, s2);
+  }
+};
