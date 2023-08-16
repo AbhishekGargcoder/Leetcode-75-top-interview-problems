8ee93c2eb5428af450ebaf922bc24f8139b8a4db
Abhishek garg <98762746+AbhishekGargcoder@users.noreply.github.com>
2023-08-16 16:16:01 +0530

    Added solution - LeetHub

---
+++ Buy Stock with Transaction Fee - GFG/buy-stock-with-transaction-fee.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+  int n, fee;
+  vector<vector<long long>>dp;
+  long long f(int idx,int buy,vector<long long>&arr){
+   if(idx>=n)
+       return 0;
+ long long profit=0;
+ if(dp[idx][buy]!=-1)return dp[idx][buy];
+  if(buy)
+    profit=max(arr[idx]+f(idx+1,0,arr)-fee,0+f(idx+1,1,arr));
+      
+      
+  else
+  profit=max(-arr[idx]+f(idx+1,1,arr),0+f(idx+1,0,arr));
+      
+   return dp[idx][buy]=profit;
+      
+}
+   long long maxProfit(vector<long long>&arr) {
+   n= arr.size() ;
+    dp=vector<vector<long long>>(n+1,vector<long long >(2,-1));
+     return f(0,0,arr);
+    }
+    long long maximumProfit(vector<long long>&arr, int n, int fee) {
+       this->fee =fee;
+       return maxProfit (arr);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<long long> prices;
+        
+        for(int i=0; i<n; ++i){
+            long long x; cin>>x;
+            prices.push_back(x);
+        }
+        
+        int fee; cin>>fee;
+        
+        Solution obj;
+        cout<<obj.maximumProfit(prices, n, fee)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
