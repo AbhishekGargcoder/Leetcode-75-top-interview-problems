32e51520e713e9b536d937b57108f87ddc31ba32
Abhishek garg <98762746+AbhishekGargcoder@users.noreply.github.com>
2023-08-21 17:29:57 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (16.16%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int>dp;
+    int f(int n){
+        if(n<0) return 0;
+        if(n == 0) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] =  f(n-1) + f(n-2);
+    }
+    int climbStairs(int n) {
+        dp = vector<int>(n+1,-1);
+        return f(n);
+    }
+};
