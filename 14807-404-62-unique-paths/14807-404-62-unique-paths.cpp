367a2adec9b0de6a299fc4b995a52950d01c80eb
Abhishek garg <98762746+AbhishekGargcoder@users.noreply.github.com>
2023-08-22 13:46:02 +0530

    Time: 0 ms, Memory: 6.5 MB - LeetHub

--- 14807-404-62-unique-paths/14807-404-62-unique-paths.cpp
+++ 14807-404-62-unique-paths/14807-404-62-unique-paths.cpp
@@ -33,9 +33,21 @@ public:
         }
     
      }*/
-  dp = vector<vector<int>>(r,vector<int>(c,-1));
-    
-      return f(r-1,c-1);
+  dp = vector<vector<int>>(r,vector<int>(c,0));
+    dp[0][0]=1;
+ for(int j=0;j<c;j++)
+     dp[0][j]=1;
+ for(int i=0;i<r;i++) dp[i][0]=1;
+   for(int i=1;i<r;i++){
+     for(int j=1;j<c;j++){
+         
+        dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        
+         
+     }
+   }
+     return dp[r-1][c-1];
+    //  return f(r-1,c-1);
               
             
             
