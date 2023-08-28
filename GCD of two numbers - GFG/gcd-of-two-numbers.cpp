a67b3a264e129aa06fb85eeda103ee29ca8f31cd
Abhishek garg <98762746+AbhishekGargcoder@users.noreply.github.com>
2023-08-28 21:48:56 +0530

    Added solution - LeetHub

---
+++ GCD of two numbers - GFG/gcd-of-two-numbers.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+	public:
+    int gcd(int a, int b) 
+	{ 
+	    while(a){
+	        int temp = b%a;
+	        b = a;
+	        a = temp;
+	    }
+	    return b;
+	   
+	      
+	} 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int A, B;
+        cin >> A >> B;
+        Solution ob;
+       	cout <<  ob.gcd(A, B) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
