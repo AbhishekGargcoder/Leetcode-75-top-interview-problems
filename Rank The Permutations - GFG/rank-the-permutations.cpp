0e20a82cd32b1973c32b441316711333a46c33cd
Abhishek garg <98762746+AbhishekGargcoder@users.noreply.github.com>
2023-09-03 13:12:05 +0530

    Added solution - LeetHub

--- Rank The Permutations - GFG/rank-the-permutations.cpp
+++ Rank The Permutations - GFG/rank-the-permutations.cpp
@@ -1,47 +1,59 @@
 //{ Driver Code Starts
+// Initial Template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
 // } Driver Code Ends
-//User function template for C++
+// User function Template for C++
 
 class Solution{
-  public:
-    long long  fact(int n ){
-        long long  f = 1;
-        for(int  i =1;i<=n;i++)
-            f = (f*i);
-        return f;
+public:
+    int MOD ;
+    int fact(int n ){
+        int f = 1;
+        for(int i =1;i<=n;i++)
+            f = (f*i) %  MOD;
+        return f%MOD;
     }
-    long long  rank(string s){
+    int rank(string s){
+        MOD = 1e6+3;
+        map<char,int>mp;
         int n = s.length();
-        long long res = 0,k= 1;
+        for(int i =0;i<n;i++)
+            mp[s[i]]++;
+        for(auto c: mp) if(c.second >1)return 0;
+        int res = 0,k= 1;
+        // bool visit[256] = {0};
         for(int i =0;i<n;i++){
+            // visit[s[i]] = true;
+            int perm = fact(n-k);
             for(int j = i+1;j<n;j++){
                 if(s[j] < s[i]){
-                    res = (res + fact(n-k));
+                    res = (res + perm) % MOD;
                 }
             }
+            // cout<<res<<" ";
+            // cout<<endl;
             k++;
         }
         res = res+1;
-        return res;
-    }
-    long long findRank(string s) {
-        return rank(s);
+        return res%MOD;
     }
 };
 
 //{ Driver Code Starts.
+
 int main(){
-    int T;
-    cin>>T;
-    while(T--){
-        string s;
-        cin>>s;
-        Solution obj;
-        long long ans = obj.findRank(s);
-        cout<<ans<<endl;
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        
+        Solution ob;
+        cout<<ob.rank(S)<<endl;
     }
+    return 0;
 }
 // } Driver Code Ends
