209210957f70c273f43c293e8d9b1b5ced6fc1cc
Abhishek garg <98762746+AbhishekGargcoder@users.noreply.github.com>
2023-09-03 13:17:26 +0530

    Added solution - LeetHub

--- Rank The Permutations - GFG/rank-the-permutations.cpp
+++ Rank The Permutations - GFG/rank-the-permutations.cpp
@@ -1,62 +1,50 @@
 //{ Driver Code Starts
-// Initial Template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
 // } Driver Code Ends
-// User function Template for C++
+//User function template for C++
 
 class Solution{
-public:
-    int MOD ;
-    int fact(int n ){
-        int f = 1;
-        for(int i =1;i<=n;i++)
-            f = (f*i) %  MOD;
-        return f%MOD;
+  public:
+    long long  fact(int n ){
+        long long  f = 1;
+        for(int  i =1;i<=n;i++)
+            f = (f*i);
+        return f;
     }
-    int rank(string s){
-        MOD = 1e6+3;
-        map<char,int>mp;
+    long long  rank(string s){
         int n = s.length();
-        for(int i =0;i<n;i++)
-            mp[s[i]]++;
-        for(auto c: mp) if(c.second >1)return 0;
-        int res = 0,k= 1;
-        // bool visit[256] = {0};
-        int perm = fact(n-1);
+        long long perm = fact(n-1);
+        long long res = 0,k= 1;
         for(int i =0;i<n;i++){
-            // visit[s[i]] = true;
-            // int perm = fact(n-k);
             for(int j = i+1;j<n;j++){
                 if(s[j] < s[i]){
-                    res = (res + perm) % MOD;
+                    res = (res + perm);
                 }
             }
-            if(n-k)
-            perm = perm/(n-k);
-            // cout<<res<<" ";
-            // cout<<endl;
+            if(n-k!=0)
+            perm =(perm/(n-k));
             k++;
         }
         res = res+1;
-        return res%MOD;
+        return res;
+    }
+    long long findRank(string s) {
+        return rank(s);
     }
 };
 
 //{ Driver Code Starts.
-
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        string S;
-        cin>>S;
-        
-        Solution ob;
-        cout<<ob.rank(S)<<endl;
+    int T;
+    cin>>T;
+    while(T--){
+        string s;
+        cin>>s;
+        Solution obj;
+        long long ans = obj.findRank(s);
+        cout<<ans<<endl;
     }
-    return 0;
 }
 // } Driver Code Ends
