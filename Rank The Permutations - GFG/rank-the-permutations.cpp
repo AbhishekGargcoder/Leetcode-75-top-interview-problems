8a28cee2aa3a526f7fdb55a94893980a1d5aa7a0
Abhishek garg <98762746+AbhishekGargcoder@users.noreply.github.com>
2023-09-03 13:02:35 +0530

    Added solution - LeetHub

--- Rank The Permutations - GFG/rank-the-permutations.cpp
+++ Rank The Permutations - GFG/rank-the-permutations.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 class Solution{
   public:
-      int MOD ;
     long long  fact(int n ){
         long long  f = 1;
         for(int  i =1;i<=n;i++)
@@ -15,30 +14,20 @@ class Solution{
         return f;
     }
     long long  rank(string s){
-        MOD = 1e6+3;
-        map<char,int>mp;
         int n = s.length();
-        // for(int i =0;i<n;i++)
-        //     mp[s[i]]++;
-        // for(auto c: mp) if(c.second >1)return 0;
         long long res = 0,k= 1;
-        // bool visit[256] = {0};
         for(int i =0;i<n;i++){
-            // visit[s[i]] = true;
             for(int j = i+1;j<n;j++){
                 if(s[j] < s[i]){
                     res = (res + fact(n-k));
                 }
             }
-            // cout<<res<<" ";
-            // cout<<endl;
             k++;
         }
         res = res+1;
         return res;
     }
     long long findRank(string s) {
-        //code here
         return rank(s);
     }
 };
