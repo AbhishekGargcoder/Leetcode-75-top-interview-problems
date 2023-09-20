495754704243e51f83b0e98d9fd779c10b3ad87c
Abhishek garg <98762746+AbhishekGargcoder@users.noreply.github.com>
2023-09-20 23:02:09 +0530

    Time: 222 ms (30.13%), Space: 98.8 MB (38.49%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -10,32 +10,17 @@ public:
         int s =0,e =0 ,curr_sum = 0,res = INT_MAX;
         while(e<n){
             curr_sum += arr[e];
-            e++;
             while(curr_sum > x && s<e){
                 curr_sum -= arr[s];
                 s++;
             }
             if(curr_sum == x){
-                res = min(res , n - (e-s));
+                cout<<e<<" "<<s<<endl;
+                res = min(res , n - (e-s+1));
             }
+            e++;
+
         }
         return res == INT_MAX ? -1 : res;
     }
 };
-/*
-        while (rightIndex < n) {
-            currentSum += nums[rightIndex];
-            rightIndex++;
-            while (currentSum > target && leftIndex < n) {
-                currentSum -= nums[leftIndex];
-                leftIndex++;
-            }
-            if (currentSum == target)
-                minOperations = min(minOperations, n - (rightIndex - leftIndex));
-        }
-        return (minOperations == INT_MAX) ? -1 : minOperations; // Return the minimum operations or -1 if not possible
-    }    
-            
-
-*/
-
