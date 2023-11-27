74d0a83ace2561cb96ae764458ad819ffae7176f
Abhishek garg <98762746+AbhishekGargcoder@users.noreply.github.com>
2023-11-27 12:15:14 +0530

    Added solution - LeetHub

---
+++ Majority Vote - GFG/majority-vote.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> Solve(int n, vector<int>&arr) {
+        map<int,int>freq;
+        int cnt = 0;
+        vector<int>ans;
+        for(auto &x:arr) freq[x]++;
+        for(auto &p : freq) if(p.second > n/3) ans.push_back(p.first); 
+        if(ans.size()==0) return {-1};
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            a.push_back(x);
+        }
+        Solution obj;
+
+        vector<int> res = obj.Solve(n, a);
+
+        for (auto x : res) cout << x << " ";
+
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
