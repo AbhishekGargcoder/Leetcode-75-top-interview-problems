8d917e35bb03875c732e71c7782b01127dce1621
Abhishek garg <98762746+AbhishekGargcoder@users.noreply.github.com>
2023-11-27 12:10:11 +0530

    Time: 10 ms (56.85%), Space: 16.2 MB (68.57%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -5,8 +5,7 @@ public:
         int n = arr.size(),cnt = 0;
         vector<int>ans;
         for(auto &x:arr) freq[x]++;
-        for(auto &p : freq) if(p.second > n/3) ans.push_back(p.first);
-        
+        for(auto &p : freq) if(p.second > n/3) ans.push_back(p.first); 
         return ans;
     }
 };
